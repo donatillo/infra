a57112c4df5cabd92891124bc88b6fb23d83e718
Andre Wagner <andre.nho@gmail.com>
2019-02-10 14:02:14 -0200

    .

---
+++ backend/main.tf
@@ -0,0 +1,65 @@
+#
+# VARIABLES
+#
+
+variable "access_key" {}
+variable "secret_key" {}
+variable "basename" {}
+variable "region" {
+    default = "us-east-1"
+}
+
+# 
+# SETUP
+#
+
+provider "aws" {
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+    region     = "${var.region}"
+}
+
+
+# 
+# S3 bucket
+# 
+
+data "aws_caller_identity" "current" {}
+
+data "template_file" "policy_devl" {
+	template 	= "${file("policy.json")}"
+	vars {
+		user    = "${data.aws_caller_identity.current.arn}"
+        env     = "devl"
+        basename = "${var.basename}"
+	}
+}
+
+resource "aws_s3_bucket" "backend_devl" {
+    bucket      = "${var.basename}-terraform-devl"
+	acl         = "private"
+    policy      = "${data.template_file.policy_devl.rendered}"
+	versioning {
+		enabled = true
+	}
+}
+
+data "template_file" "policy_master" {
+	template 	= "${file("policy.json")}"
+	vars {
+		user    = "${data.aws_caller_identity.current.arn}"
+        env     = "master"
+        basename = "${var.basename}"
+	}
+}
+
+resource "aws_s3_bucket" "backend_master" {
+    bucket      = "${var.basename}-terraform-master"
+	acl         = "private"
+    policy      = "${data.template_file.policy_master.rendered}"
+	versioning {
+		enabled = true
+	}
+}
+
+# vim:ts=4:sw=4:sts=4:expandtab:syntax=conf
---
+++ create_from_scratch.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ ! -f "secrets.auto.tfvars" ]; then
+    echo "Please create a file called 'secrets.auto.tfvars' with the following format:"
+    echo "-----------------------"
+    echo "access_key = \"MY_AWS_ACCESS_KEY\""
+    echo "secret_key = \"MY_AWS_SECRET_KEY\""
+    echo "domain     = \"MY_DOMAIN\""
+    echo "basename   = \"MY_APP\'S_BASE_NAME\""
+    echo "-----------------------"
+    exit 1
+fi
+
+echo
+echo '#'
+echo '# creating backend...'
+echo '#'
+echo
+cd backend
+terraform init
+terraform apply
+cd ..
+echo
+
+echo 'Creation completed.'
+
+# vim:st=4:sts=4:sw=4:expandtab
