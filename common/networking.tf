04549fe025cfee75a166b95cf2552271cd9e518b
Andre Wagner <andre.nho@gmail.com>
2019-02-10 16:34:56 -0200

    .

---
+++ common/networking.tf
@@ -0,0 +1,73 @@
+# VPC
+
+resource "aws_vpc" "main" {
+    cidr_block  = "10.0.0.0/16"
+    
+    tags {
+        Name        = "${var.basename}-vpc"
+        Creator     = "init-aws"
+        Description = "Main VPC"
+    }
+}
+
+# subnet (public)
+
+resource "aws_subnet" "public" {
+    vpc_id      = "${aws_vpc.main.id}"
+    cidr_block  = "10.0.1.0/24"
+
+    tags {
+        Name        = "${var.basename}-subnet-public"
+        Creator     = "init-aws"
+        Description = "Main public subnet"
+    }
+}
+
+# subnet (private)
+
+resource "aws_subnet" "private" {
+    vpc_id      = "${aws_vpc.main.id}"
+    cidr_block  = "10.0.0.0/24"
+
+    tags {
+        Name        = "${var.basename}-subnet-private"
+        Creator     = "init-aws"
+        Description = "Main private subnet"
+    }
+}
+
+# internet gateway (associated to the public subnet)
+
+resource "aws_internet_gateway" "ig_public_subnet" {
+    vpc_id      = "${aws_vpc.main.id}"
+
+    tags {
+        Name        = "${var.basename}-ig"
+        Creator     = "init-aws"
+        Description = "Internet gateway for public subnet"
+    }
+}
+
+resource "aws_route_table_association" "x" {
+    subnet_id       = "${aws_subnet.public.id}"
+    route_table_id  = "${aws_internet_gateway.ig_public_subnet.id}"
+}
+
+# route table (associated to the internet gateway)
+
+resource "aws_route_table" "rt" {
+    vpc_id      = "${aws_vpc.main.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.ig_public_subnet.id}"
+    }
+
+    tags {
+        Name        = "${var.basename}-rt"
+        Creator     = "init-aws"
+        Description = "Route table for internet gateway"
+    }
+}
+
+# vim:ts=4:sw=4:sts=4:expandtab:syntax=conf
--- common/variables.tf
+++ common/variables.tf
@@ -10,4 +10,10 @@ variable "region" {
     default = "us-east-1"
 }
 
+provider "aws" {
+    access_key      = "${var.access_key}"
+    secret_key      = "${var.secret_key}"
+    region          = "${var.region}"
+}
+
 # vim:ts=4:sw=4:sts=4:expandtab:syntax=conf
