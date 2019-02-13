f92f89ad139788e9cbd5673866802775252b58ae
Andre Wagner <andre.nho@gmail.com>
2019-02-13 00:12:02 -0200

    .

--- common/network/pubsubnet/main.tf
+++ common/network/pubsubnet/main.tf
@@ -9,6 +9,7 @@ variable "vpc_id"            {}
 resource "aws_subnet" "public" {
     vpc_id      = "${var.vpc_id}"
     cidr_block  = "${var.cidr_block}"
+    availability_zone = "us-east-1${var.availability_zone}"
 
     tags {
         Name        = "${var.basename}-subnet-public-${var.env}-${var.availability_zone}"
