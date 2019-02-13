10361d9c1356a9751c0522c4dba5bf62fd6ccd1d
Andre Wagner <andre.nho@gmail.com>
2019-02-13 02:14:19 +0000

    .

--- common/network/main.tf
+++ common/network/main.tf
@@ -21,7 +21,7 @@ module "subnet-az-a" {
     env               = "${var.env}"
     basename          = "${var.basename}"
     availability_zone = "a"
-    cidr_block        = "10.0.1.0/24"
+    cidr_block        = "10.0.2.0/24"
     vpc_id            = "${aws_vpc.main.id}"
 }
 
@@ -30,20 +30,34 @@ module "subnet-az-b" {
     env               = "${var.env}"
     basename          = "${var.basename}"
     availability_zone = "a"
-    cidr_block        = "10.0.2.0/24"
+    cidr_block        = "10.0.3.0/24"
     vpc_id            = "${aws_vpc.main.id}"
 }
 
 # subnet (private)
 
-resource "aws_subnet" "private" {
+
+resource "aws_subnet" "private-a" {
     vpc_id      = "${aws_vpc.main.id}"
     cidr_block  = "10.0.0.0/24"
 
     tags {
-        Name        = "${var.basename}-subnet-private-${var.env}"
+        Name        = "${var.basename}-subnet-private-${var.env}-a"
         Creator     = "init-aws"
-        Description = "Main private subnet"
+        Description = "Main private subnet - zone a"
+        Environment = "${var.env}"
+    }
+}
+
+
+resource "aws_subnet" "private-b" {
+    vpc_id      = "${aws_vpc.main.id}"
+    cidr_block  = "10.0.1.0/24"
+
+    tags {
+        Name        = "${var.basename}-subnet-private-${var.env}-b"
+        Creator     = "init-aws"
+        Description = "Main private subnet - zone b"
         Environment = "${var.env}"
     }
 }
