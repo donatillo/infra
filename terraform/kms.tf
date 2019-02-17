506e2efba4185e54735f352d82cc9855e919e1a7
Andre Wagner <andre.nho@gmail.com>
2019-02-17 17:17:05 -0300

    .

--- terraform/kms.tf
+++ terraform/kms.tf
@@ -1,5 +1,24 @@
+# find users
+data "aws_iam_user" "root" {
+    user_name = "root"
+}
+
+data "aws_caller_identity" "ci" {}
+
+# parse policy file
+data "template_file" "policy" {
+    template    = "${file("keypolicy.json")}"
+    vars {
+        root    = "${data.aws_iam_user.root.arn}"
+        ci      = "${data.aws_caller_identity.ci.arn}"
+		dynamo  = "${aws_iam_user.dynamo.arn}"
+    }
+}
+
+# create key
 resource "aws_kms_key" "key" {
     description = "Main KMS for accessing SSM"
+    policy      = "${data.template_file.policy.rendered}"
 }
 
 resource "aws_kms_alias" "a" {
